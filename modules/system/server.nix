ce726857cdf9749183aa06edf251a3aabc18a60d
Térence Chateigné <terence.chateigne@posteo.net>
2024-10-10 23:32:42 +0200

    fix(openiscsi): reenable service

--- modules/system/server.nix
+++ modules/system/server.nix
@@ -1,9 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   environment = {
     systemPackages = with pkgs; [
       openiscsi
     ];
+  };
+
+  services.openiscsi = {
+    enable = true;
+    name = "iqn.2016-04.com.open-iscsi:" + config.networking.hostName;
   };
 }
