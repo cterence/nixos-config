7537dae9056505983b500f702f18ad2e41dbfd4c
Térence Chateigné <terence.chateigne@posteo.net>
2024-10-13 18:35:36 +0200

    chore: comment out unused code

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,11 +7,11 @@
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
-  modifications = final: prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
-  };
+  # modifications = final: prev: {
+  # example = prev.example.overrideAttrs (oldAttrs: rec {
+  # ...
+  # });
+  # };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
