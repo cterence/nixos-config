63d244691d5b3b2ad8d4fd769d91aaeff37a0485
Térence Chateigné <terence.chateigne@posteo.net>
2024-10-15 20:48:25 +0200

    feat(server): add tmpfiles rule

--- modules/system/server.nix
+++ modules/system/server.nix
@@ -1,14 +1,18 @@
 { pkgs, config, ... }:
 
 {
+  # Needed for Longhorn
   environment = {
     systemPackages = with pkgs; [
       openiscsi
     ];
   };
-
   services.openiscsi = {
     enable = true;
     name = "iqn.2016-04.com.open-iscsi:" + config.networking.hostName;
   };
+  # https://github.com/longhorn/longhorn/issues/2166#issuecomment-1740179416
+  systemd.tmpfiles.rules = [
+    "L+ /usr/local/bin - - - - /run/current-system/sw/bin/"
+  ];
 }
