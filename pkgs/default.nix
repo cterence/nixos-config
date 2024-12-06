06554cd510b4a7a3579149822f6dd569ebd16330
Térence Chateigné <terence.chateigne@theodo.com>
2024-12-06 11:31:46 +0100

    feat(fw): use nixpkgs version of google-chat-linux after merge

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,6 @@
 pkgs: {
   aws-sso-util = pkgs.callPackage ./aws-sso-util.nix { };
   baywatch = pkgs.callPackage ./baywatch.nix { };
-  google-chat-linux = pkgs.callPackage ./google-chat-linux.nix { };
   guacamole = pkgs.callPackage ./guacamole.nix { };
   openfortivpn-webview = pkgs.callPackage ./openfortivpn-webview.nix { };
   playground = pkgs.callPackage ./playground.nix { };
