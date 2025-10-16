90a4b4558e86b6610b2dadd4c98197b814baf832
Térence Chateigné <terence@dejablue.energy>
2025-10-16 16:44:14 +0200

    chore: fmt

--- modules/user/vscode.nix
+++ modules/user/vscode.nix
@@ -4,11 +4,9 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscode.overrideAttrs (previousAttrs: {
-      postFixup =
-        previousAttrs.postFixup
-        + ''
-          sed -i -e "s|StartupWMClass=.*|StartupWMClass=code-url-handler|" $out/share/applications/code-url-handler.desktop
-        '';
+      postFixup = previousAttrs.postFixup + ''
+        sed -i -e "s|StartupWMClass=.*|StartupWMClass=code-url-handler|" $out/share/applications/code-url-handler.desktop
+      '';
     });
   };
 }
