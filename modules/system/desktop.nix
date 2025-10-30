fbd6be4c07b43566464c32728b55ea9354d261ea
Térence Chateigné <terence.chateigne@posteo.net>
2025-10-30 11:36:11 +0100

    chore: naming changes

--- modules/system/desktop.nix
+++ modules/system/desktop.nix
@@ -97,7 +97,7 @@
       [
         noto-fonts
         noto-fonts-cjk-sans
-        noto-fonts-emoji
+        noto-fonts-color-emoji
         liberation_ttf
         fira-code-symbols
         mplus-outline-fonts.githubRelease
