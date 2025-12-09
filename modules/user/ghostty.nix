9d8387c7426ec83d1f1c87656651a7bb0fca5f6f
Térence Chateigné <terence.chateigne@posteo.net>
2025-12-09 19:46:53 +0100

    fix: ghostty quotes

--- modules/user/ghostty.nix
+++ modules/user/ghostty.nix
@@ -1,6 +1,8 @@
 {
+  # https://github.com/ghostty-org/ghostty/discussions/8899#discussioncomment-14717979
   home.file.".config/ghostty/config".text = ''
     font-family = FiraCode Nerd Font
     theme = Abernathy
+    env = GTK_IM_MODULE=simple
   '';
 }
