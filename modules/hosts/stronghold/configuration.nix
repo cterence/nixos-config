e59133aac1625b6adb0ce4cd543dde4f9ece2e5a
Térence Chateigné <terence.chateigne@posteo.net>
2026-03-08 19:38:55 +0100

    migrate stronghold

---
+++ modules/hosts/stronghold/configuration.nix
@@ -0,0 +1,29 @@
+{ inputs, ... }:
+{
+  flake.modules.nixos.stronghold = {
+    imports =
+      with inputs.self.modules.nixos;
+      [
+        audio
+        bluetooth
+        common-settings
+        common-services
+        cli-tools
+        docker
+        games
+        home-manager
+        homelab-client-cert
+        kde
+        printing
+        systemd-boot
+        terence
+        tailscale
+      ]
+      ++ [
+        inputs.sops-nix.nixosModules.sops
+      ];
+    networking.hostName = "stronghold";
+
+    system.stateVersion = "25.11";
+  };
+}
---
+++ modules/hosts/stronghold/hardware.nix
@@ -0,0 +1,42 @@
+{
+  flake.modules.nixos.stronghold = {
+    boot = {
+      kernelParams = [ "acpi_backlight=video" ];
+      kernelModules = [
+        "kvm-amd"
+        "i2c-dev"
+      ];
+      extraModulePackages = [ ];
+      initrd = {
+        availableKernelModules = [
+          "nvme"
+          "xhci_pci"
+          "ahci"
+          "usbhid"
+          "usb_storage"
+          "sd_mod"
+        ];
+        kernelModules = [ ];
+      };
+    };
+
+    fileSystems = {
+      "/" = {
+        device = "/dev/disk/by-uuid/83263226-8908-4963-8344-20f96234990c";
+        fsType = "ext4";
+      };
+      "/boot" = {
+        device = "/dev/disk/by-uuid/7F1A-55C3";
+        fsType = "vfat";
+      };
+    };
+
+    swapDevices = [ { device = "/dev/disk/by-uuid/9fd4feb3-15f8-4d6b-a9e8-70a383f1f362"; } ];
+
+    nixpkgs.hostPlatform = "x86_64-linux";
+    hardware = {
+      cpu.amd.updateMicrocode = true;
+      enableRedistributableFirmware = true;
+    };
+  };
+}
