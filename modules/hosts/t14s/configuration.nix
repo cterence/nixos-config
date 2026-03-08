79eee42370cc01f2a88d1d61164d66a4cd066c5e
Térence Chateigné <terence.chateigne@posteo.net>
2026-03-08 19:17:25 +0100

    add t14s

---
+++ modules/hosts/t14s/configuration.nix
@@ -0,0 +1,29 @@
+{ inputs, ... }:
+{
+  flake.modules.nixos.t14s = {
+    imports =
+      with inputs.self.modules.nixos;
+      [
+        audio
+        bluetooth
+        common-settings
+        common-services
+        cli-tools
+        docker
+        games
+        home-manager
+        homelab-client-cert
+        kde
+        printing
+        systemd-boot
+        terence
+        tailscale
+      ]
+      ++ [
+        inputs.sops-nix.nixosModules.sops
+      ];
+    networking.hostName = "t14s";
+
+    system.stateVersion = "25.11";
+  };
+}
---
+++ modules/hosts/t14s/hardware.nix
@@ -0,0 +1,38 @@
+{ inputs, ... }:
+{
+  flake.modules.nixos.t14s = {
+    imports = [
+      inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14s-amd-gen1
+    ];
+
+    boot = {
+      initrd.availableKernelModules = [
+        "nvme"
+        "ehci_pci"
+        "xhci_pci"
+        "usb_storage"
+        "sd_mod"
+        "rtsx_pci_sdmmc"
+      ];
+      kernelModules = [ "kvm-amd" ];
+      extraModulePackages = [ ];
+    };
+
+    fileSystems = {
+      "/" = {
+        device = "/dev/disk/by-uuid/b7abd0b2-b567-46a0-91f7-63f7e63f789b";
+        fsType = "ext4";
+      };
+      "/boot" = {
+        device = "/dev/disk/by-uuid/F0B2-CF53";
+        fsType = "vfat";
+      };
+    };
+
+    swapDevices = [ ];
+
+    nixpkgs.hostPlatform = "x86_64-linux";
+    hardware.cpu.amd.updateMicrocode = true;
+    hardware.enableRedistributableFirmware = true;
+  };
+}
