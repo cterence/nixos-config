e1c9ff9a961c4ef09ec9120a88a5650714151d6a
Térence Chateigné <terence.chateigne@posteo.net>
2026-03-10 21:46:40 +0100

    feat: pinentry gpg

--- modules/services/gpg.nix
+++ modules/services/gpg.nix
@@ -8,9 +8,10 @@
           enable = true;
           enableSshSupport = true;
           enableZshIntegration = true;
-          # pinentry is a collection of simple PIN or passphrase dialogs used for
-          # password entry
-          pinentry.package = pkgs.pinentry-qt;
+          pinentry = {
+            package = pkgs.kwalletcli;
+            program = "pinentry-kwallet";
+          };
         };
       };
       programs.gpg = {
