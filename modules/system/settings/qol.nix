704fe7a4c1bef1109b4c966ae47750046cb404c5
Térence Chateigné <terence.chateigne@posteo.net>
2026-03-14 00:33:08 +0100

    fwupd

--- modules/system/settings/qol.nix
+++ modules/system/settings/qol.nix
@@ -3,6 +3,7 @@
     # Allow /bin/bash to be used as a shell for scripts
     # https://discourse.nixos.org/t/add-bin-bash-to-avoid-unnecessary-pain/5673/63
     services.envfs.enable = true;
+    services.fwupd.enable = true;
     programs.nix-ld.enable = true;
   };
 }
