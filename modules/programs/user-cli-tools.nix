a00f805b903e7034a5fdfd2d7ce238fc9721d2fc
Térence Chateigné <terence.chateigne@posteo.net>
2026-03-24 22:55:48 +0100

    add aws credentials

--- modules/programs/user-cli-tools.nix
+++ modules/programs/user-cli-tools.nix
@@ -1,57 +1,67 @@
+{ inputs, ... }:
 {
   flake.modules.homeManager.cli-tools =
-    { pkgs, ... }:
+    { pkgs, config, ... }:
     {
-      home.packages = with pkgs; [
-        age
-        argocd
-        bat
-        btop
-        cmctl
-        docker-buildx
-        dust
-        entr
-        eza
-        fastfetch
-        fd
-        ffmpeg
-        glab
-        htop
-        ipcalc
-        jq
-        jqp
-        kind
-        kopia
-        krew
-        kubectl
-        kubernetes-helm
-        manix
-        nil
-        nix-init
-        nix-inspect
-        nix-prefetch
-        nix-update
-        nixd
-        nixfmt
-        nmap
-        nurl
-        nvtopPackages.amd
-        p7zip
-        postgresql
-        pv-migrate
-        rclone
-        socat
-        sops
-        tenv
-        tfautomv
-        tig
-        unzip
-        velero
-        versitygw
-        yq-go
-        yt-dlp
-        zip
-      ];
+      home = {
+        packages = with pkgs; [
+          age
+          argocd
+          bat
+          btop
+          cmctl
+          docker-buildx
+          dust
+          entr
+          eza
+          fastfetch
+          fd
+          ffmpeg
+          glab
+          htop
+          ipcalc
+          jq
+          jqp
+          kind
+          kopia
+          krew
+          kubectl
+          kubernetes-helm
+          manix
+          nil
+          nix-init
+          nix-inspect
+          nix-prefetch
+          nix-update
+          nixd
+          nixfmt
+          nmap
+          nurl
+          nvtopPackages.amd
+          p7zip
+          postgresql
+          pv-migrate
+          rclone
+          socat
+          sops
+          tenv
+          tfautomv
+          tig
+          unzip
+          velero
+          versitygw
+          yq-go
+          yt-dlp
+          zip
+        ];
+      };
+
+      sops.secrets = {
+        aws-credentials = {
+          sopsFile = "${inputs.secrets}/aws-credentials.yaml";
+          path = config.home.homeDirectory + "/.aws/credentials";
+        };
+      };
 
       programs = {
         awscli = {
@@ -69,6 +79,10 @@
               sso_account_id = "964066691632";
               sso_role_name = "AdministratorAccess";
               region = "eu-west-3";
+            };
+            "profile versitygw" = {
+              region = "eu-west-3";
+              endpoint_url = "https://versity.terence.cloud";
             };
           };
         };
@@ -141,6 +155,5 @@
           };
         };
       };
-
     };
 }
