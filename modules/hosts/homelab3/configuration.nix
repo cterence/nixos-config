ec06f740d385d4d3d90fe4d42a19991aa5c43a20
Térence Chateigné <terence.chateigne@posteo.net>
2026-03-27 23:23:07 +0100

    tighter nfs config

--- modules/hosts/homelab3/configuration.nix
+++ modules/hosts/homelab3/configuration.nix
@@ -37,8 +37,8 @@
         nfs.server = {
           enable = true;
           exports = ''
-            /export                 192.168.1.0/24  (rw,fsid=0,no_subtree_check)
-            /export/mx500-homelab3  192.168.1.54/32 (rw,nohide,no_subtree_check,root_squash,all_squash,anonuid=1000,anongid=100)
+            /export                 192.168.1.54/32(rw,fsid=0,no_subtree_check,all_squash,anonuid=1000,anongid=100)
+            /export/mx500-homelab3  192.168.1.54/32(rw,nohide,no_subtree_check,all_squash,anonuid=1000,anongid=100)
           '';
         };
       };
