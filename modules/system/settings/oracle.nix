2d4b046aaee05f485c9cff107fd0927571b2469b
Térence Chateigné <terence.chateigne@posteo.net>
2026-04-10 23:06:37 +0200

    udp perf

--- modules/system/settings/oracle.nix
+++ modules/system/settings/oracle.nix
@@ -1,30 +1,55 @@
 {
-  flake.modules.nixos.settings-oracle = {
-    boot = {
-      initrd.availableKernelModules = [
-        "virtio_pci"
-        "virtio_scsi"
-        "usbhid"
-        "nvme"
-      ];
-      kernel.sysctl = {
-        "net.ipv4.ip_forward" = 1;
-        "net.ipv6.conf.all.forwarding" = 1;
+  flake.modules.nixos.settings-oracle =
+    { pkgs, ... }:
+    {
+      boot = {
+        initrd.availableKernelModules = [
+          "virtio_pci"
+          "virtio_scsi"
+          "usbhid"
+          "nvme"
+        ];
+        kernel.sysctl = {
+          "net.ipv4.ip_forward" = 1;
+          "net.ipv6.conf.all.forwarding" = 1;
+        };
       };
-    };
 
-    services.openssh = {
-      enable = true;
-      settings.PasswordAuthentication = false;
-      settings.PermitRootLogin = "yes";
-    };
+      systemd = {
+        network.enable = false;
+        services.tailscale-udp-optimizations = {
+          description = "Tailscale UDP throughput optimizations";
 
-    systemd.network.enable = false;
-    networking = {
-      useDHCP = true;
-      dhcpcd.enable = true;
-      useNetworkd = false;
+          # Ensure this runs after the network is up
+          after = [ "network.target" ];
+          wantedBy = [ "multi-user.target" ];
+
+          serviceConfig = {
+            Type = "oneshot";
+            RemainAfterExit = true;
+          };
+
+          script = ''
+            # Find the primary network interface
+            NETDEV=$(${pkgs.iproute2}/bin/ip -o route get 8.8.8.8 | cut -f 5 -d " ")
+
+            echo "Applying UDP offload optimizations to $NETDEV..."
+            ${pkgs.ethtool}/bin/ethtool -K "$NETDEV" rx-udp-gro-forwarding on rx-gro-list off
+          '';
+        };
+      };
+
+      services.openssh = {
+        enable = true;
+        settings.PasswordAuthentication = false;
+        settings.PermitRootLogin = "yes";
+      };
+
+      networking = {
+        useDHCP = true;
+        dhcpcd.enable = true;
+        useNetworkd = false;
+      };
+      security.sudo.wheelNeedsPassword = false;
     };
-    security.sudo.wheelNeedsPassword = false;
-  };
 }
