44fa3aa75fe495f316af255756b5429a829f624e
Térence Chateigné <terence.chateigne@posteo.net>
2026-04-11 17:42:31 +0200

    fix(hm3): fstype none for nfs

--- modules/hosts/homelab3/hardware.nix
+++ modules/hosts/homelab3/hardware.nix
@@ -37,7 +37,7 @@
       };
       "/export/mx500-homelab3" = {
         device = "/mnt/mx500-02/k8s-data";
-        fsType = "ext4";
+        fsType = "none";
         options = [ "bind" ];
         depends = [ "/mnt/mx500-02" ];
       };
